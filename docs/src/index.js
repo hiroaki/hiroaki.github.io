d0e1d5155b71c028894add80a08cb6409f4350a3
WATANABE Hiroaki <176736+hiroaki@users.noreply.github.com>
2026-05-10 16:51:10 +0900

    replace with new layout

--- docs/src/index.js
+++
@@ -1,4 +0,0 @@
-export { createTiliaCore } from "./core/boot.js";
-export { createBaseMap } from "./map/base.js";
-export { installMapControl, createPanel, createButton, createSelect } from "./map/controls.js";
-
